220762c680865502e68ba7da497ba73d38269333
Johnny Siu <siujohnny@gmail.com>
2018-08-06 17:15:13 -0700

    [edit]change home page img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ class Home extends Component{
     return(
       <div>
         <h1>Hi I'm Eugene and I like to cook.</h1>
-        <img src={eugene} alt=""/>
+        <img src="https://i.redditmedia.com/Hdlj8VTIhajl6nbd3AFfsf_ZzM9ozuE2lhgyODpONbU.jpg?fit=crop&crop=faces%2Centropy&arh=2&w=640&s=6ae4d20af1876cb1aac4ba002a2b0582" alt=""/>
         <hr/>
         <h2>Follow me on IG!</h2>
         <a href="https://www.instagram.com/you_jean_cooks/"><img className="ig" src={ig} alt =""/></a>
